2c1bf1a30ec9d20da911944845a9b0511c617968
Raffael M. Paranhos <raffaelmp@id.uff.br>
2021-03-27 18:42:23 -0300

    Update script.py

--- Atividade 1/script.py
+++ Atividade 1/script.py
@@ -1,5 +1,7 @@
 import re
 import itertools
+import collections
+
 #Parte 1
 #O arquivo com as cartas é um único TXT contém uma marcação ' ","------------------------- 1 =' para indicar o início de cada carta.
 #Seu script deve conter: a leitura do arquivo fonte, o tratamento com expressões regulares e a escrita das cartas tratadas. 
@@ -25,7 +27,12 @@ def trata_carta(arq):
         #o lixo deixado pelo processo de escrita das cartas (","),
         #e o termo &nbsp; que aparece em algumas cartas (&nbsp;(1[A-z]:[0-9])*)
         remocao_regex = r'------------------------- *[0-9]* = |\[sketch [A-z]\]|","|&nbsp;(1[A-z]:[0-9])*'
-        f_tratado.write(re.sub(remocao_regex, r'', m.group(0)))
+        texto_limpo = re.sub(remocao_regex, r'', m.group(0))
+
+        #e o termo &nbsp; que aparece em algumas cartas (&nbsp;(1[A-z]:[0-9])*)
+        remocao_regex_2 = r'\.[0-9]'
+        f_tratado.write(re.sub(remocao_regex_2, r'.', texto_limpo))
+
         count += 1
 
 #Parte 2:
@@ -43,7 +50,8 @@ def tokeniza_cartas(diretorio,n_arq,k):
     print(texto)    
     print(BPE(texto,k))
 
-def BPE(texto, k):
+#implementacao feita por mim do bpe, antes de saber que podiamos utilizar o codigo dado em aula
+def bpe_raffael(texto, k):
     #processo que cria o dicionario e o vocabulario
     dicionario = {}
     vocabulario = ['_']
@@ -103,7 +111,68 @@ def BPE(texto, k):
                     dicionario.pop(chave)
                     dicionario[chave_token] = valor
     return vocabulario, dicionario
+
+#Parte 2:
+#Na segunda parte, você deve executar os algoritmos de tokenização BPE e WordPiece vistos na aula, ambos **implementados** por você. 
+#Além do script, você deve informar quantos tokens foram encontrados por cada abordagem e 
+#apontar exemplos de possíveis diferenças que cada abordagem pode ter encontrado. 
+#Separe 70% das cartas para fazer a criação dos dicionários de tokenização e 30% para fazer o teste da sua implementação 
+#(os 30% não podem ser usados ao criar o dicionário). Caso o processo fique lento (Wordpiece pode ser lento), 
+#você pode escolher um subconjunto das cartas.
+
+#Implementacao do bpe
+def get_stats(vocab):
+    pairs = collections.defaultdict(int)
+    for word, freq in vocab.items():
+        symbols = word.split()
+        for i in range (len(symbols)-1):
+            pairs[symbols[i],symbols[i+1]] += freq
+    return pairs        
+
+def merge_vocab(pair, v_in):
+    v_out = {}
+    bigram = re.escape(' '.join(pair))
+    p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+    for word in v_in:
+        w_out = p.sub(''.join(pair), word)
+        v_out[w_out] = v_in[word]
+    return v_out
+
+def bpe(vocab, num_merges):
+    print('Iniciando BPE')
+    best_list = []
+    for i in range(num_merges):
+        pairs = get_stats(vocab)
+        best = max(pairs, key=pairs.get)
+        vocab = merge_vocab(best, vocab)
+        best_list.append(best)
+    print('BPE Finalizado')
+    return vocab, best_list
+
+
+def tokeniza_carta_bpe(diretorio,n_arq,k):
+    texto = " "
+    for i in range(0, n_arq):
+        f = open(diretorio + str(i) + ".txt", "r", encoding="utf-8")
+        texto = texto + f.read() + " "
+    dicionario = {}   
+    print("Inciando processo de criacao do dicionario")
+    for palavra in texto.split():
+        if " ".join(palavra) + ' </w>' in dicionario.keys():
+            dicionario[" ".join(palavra) + ' </w>'] = dicionario[" ".join(palavra) + ' </w>'] + 1
+        else:
+            dicionario[" ".join(palavra) + ' </w>'] = 1
+    vocab, best_list = bpe(dicionario, k)
+    print('Escrevendo resultado do BPE num arquivo')
+    f = open(diretorio + "vocab.txt", "w", encoding="utf-8")
+    f.write("Parametros: n_arq = " + str(n_arq) + ", k = " + str(k) + "\n" + str(vocab))
+    print('vocab.txt contem o vocabulario')    
+    f = open(diretorio + "best_list.txt", "w", encoding="utf-8")
+    f.write("Parametros: n_arq = " + str(n_arq) + ", k = " + str(k) + "\n" + str(best_list))
+    print('best_list.txt contem a lista de todos os "bests", pares que sao encontrados mais vezes em uma iteracao')    
     
 #trata_carta('cartas_van_gogh_segunda.txt')    
 #print(BPE("low low low low low lowest lowest newer newer newer newer newer newer wider wider wider new new", 30))
-tokeniza_cartas('cartas_tratadas/', 20, 30)
+#tokeniza_cartas('cartas_tratadas/', 20, 30)
+#bpe({'l o w </w>':5, 'l o w e s t </w>':2, 'n e w e r </w>':6,'w i d e r </w>':3, 'n e w </w>':2}, 3)
+tokeniza_carta_bpe('cartas_tratadas/', 700, 50)
