254f96098c4fa80fc1bdfb90a9750b6b4858c59a
Raffael M. Paranhos <raffaelmp@id.uff.br>
2021-04-16 00:09:55 -0300

    Add files via upload

--- Atividade 4/script.py
+++ Atividade 4/script.py
@@ -18,6 +18,8 @@ nltk.download('stopwords')
 en_stop = set(nltk.corpus.stopwords.words('english'))
 import gensim
 from gensim import corpora
+import tomotopy as tp
+import time
 
 def tokenize(text):
     lda_tokens = []
@@ -55,9 +57,10 @@ def main():
   test_df = pd.read_csv('jigsaw-toxic-comment-classification-challenge/test.csv')
 
   #PARA AGILIZAR
-  train_df = train_df.head(1000)
-  test_df = test_df.head(100)
-
+  train_df = train_df.head(20000)
+  test_df = test_df.head(2000)
+ 
+  tempo = time.time()
   train_data = []
   i = 0
   while i < train_df.shape[0]:
@@ -67,6 +70,7 @@ def main():
       train_data.append(tokens)
     i += 1
   print("")
+  print("Primeiros 5 itens da base de treino:")
   print(train_data[:5])
 
   test_data = []
@@ -78,27 +82,63 @@ def main():
       test_data.append(tokens)
     i += 1
   print("")
+  print("Primeiros 5 itens da base de teste:")
   print(test_data[:5])
 
   all_data = train_data + test_data
   dictionary = corpora.Dictionary(all_data)
   corpus = [dictionary.doc2bow(token, allow_update=True) for token in train_data]
   print("")
-  print(dictionary)
-
+  print("Tempo gasto no pre processamento: {}".format(time.time() - tempo))
+  tempo = time.time()
+  
   NUM_TOPICS = 5
+  print("")
+  print("Criando {} topicos utilizando o LDA do gensim".format(NUM_TOPICS))
   ldamodel = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word = dictionary, num_topics=NUM_TOPICS, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
   topics = ldamodel.print_topics(num_words=10)
-  print("")
+  print("Topicos: ")
   for topic in topics:
     print("")
     print(topic)
-  print("")
 
+  print("")
   test_bow = [dictionary.doc2bow(token, allow_update=True) for token in test_data]
-  print("")
+  print("Encaixando nos topicos o teste: ")
   print(test_data[0])
+  print("Resultado: ")
   print(ldamodel.get_document_topics(test_bow[0]))
+  print("")
+  print("Tempo gasto no LDA gensim: {}".format(time.time() - tempo))
+  tempo = time.time()
+  
+  #tomotopy
+  print("")
+  print("Criando 5 topicos utilizando o LDA do tomotopy")
+  mdl = tp.LDAModel(k=5)
+  
+  for line in test_data:
+    mdl.add_doc(line)
 
+  for i in range(0, 100, 10):
+    mdl.train(10)
+    #print('Iteration: {}\tLog-likelihood: {}'.format(i, mdl.ll_per_word))
+
+  print("Topicos:")
+  for k in range(mdl.k):
+    print("")
+    print('Top 10 words of topic #{}'.format(k))
+    print(mdl.get_topic_words(k, top_n=10))
+
+  #print("")
+  doc_inst = mdl.make_doc(test_data[0])
+  topic_dist, ll = mdl.infer(doc_inst)
+  print("Topic Distribution for Unseen Docs: ", topic_dist)
+  print("Log-likelihood of inference: ", ll)
+  print("")
+  print("Tempo gasto no LDA tomotopy: {}".format(time.time() - tempo))
+  tempo = time.time()
+  
+  #print(mdl.summary())
 
 main()  
