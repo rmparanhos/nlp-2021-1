9e73e72cecba6a46828cc806bb0ddd005fbfb183
Raffael M. Paranhos <raffaelmp@id.uff.br>
2021-04-19 20:09:32 -0300

    finalizado

--- Atividade 4/script.py
+++ Atividade 4/script.py
@@ -7,6 +7,7 @@
 #- EXTRA: Sugerir rótulos para ao menos 3 tópicos, no modelo escolhido por você.
 
 import pandas as pd
+import numpy as np
 import spacy
 from spacy.lang.en import English
 spacy.load('en_core_web_sm')
@@ -20,6 +21,9 @@ import gensim
 from gensim import corpora
 import tomotopy as tp
 import time
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+
 
 def tokenize(text):
     lda_tokens = []
@@ -57,8 +61,8 @@ def main():
   test_df = pd.read_csv('jigsaw-toxic-comment-classification-challenge/test.csv')
 
   #PARA AGILIZAR
-  train_df = train_df.head(20000)
-  test_df = test_df.head(2000)
+  train_df = train_df.head(2000)
+  test_df = test_df.head(100)
  
   tempo = time.time()
   train_data = []
@@ -98,16 +102,19 @@ def main():
   ldamodel = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word = dictionary, num_topics=NUM_TOPICS, random_state=100, update_every=1, chunksize=100, passes=10, alpha='auto', per_word_topics=True)
   topics = ldamodel.print_topics(num_words=10)
   print("Topicos: ")
+  k = 0
   for topic in topics:
     print("")
+    print('Top 10 palavras do topico #{}'.format(k))
+    k = k + 1
     print(topic)
 
   print("")
   test_bow = [dictionary.doc2bow(token, allow_update=True) for token in test_data]
   print("Encaixando nos topicos o teste: ")
-  print(test_data[0])
+  print(test_data[3])
   print("Resultado: ")
-  print(ldamodel.get_document_topics(test_bow[0]))
+  print(ldamodel.get_document_topics(test_bow[3]))
   print("")
   print("Tempo gasto no LDA gensim: {}".format(time.time() - tempo))
   tempo = time.time()
@@ -122,23 +129,53 @@ def main():
 
   for i in range(0, 100, 10):
     mdl.train(10)
-    #print('Iteration: {}\tLog-likelihood: {}'.format(i, mdl.ll_per_word))
-
+  
   print("Topicos:")
   for k in range(mdl.k):
     print("")
-    print('Top 10 words of topic #{}'.format(k))
+    print('Top 10 palavras do topico #{}'.format(k))
     print(mdl.get_topic_words(k, top_n=10))
 
-  #print("")
-  doc_inst = mdl.make_doc(test_data[0])
+  doc_inst = mdl.make_doc(test_data[3])
   topic_dist, ll = mdl.infer(doc_inst)
-  print("Topic Distribution for Unseen Docs: ", topic_dist)
-  print("Log-likelihood of inference: ", ll)
+  print("")
+  print("Encaixando nos topicos o teste: ")
+  print(test_data[3])
+  print("Resultado: ")
+  print(topic_dist)
   print("")
   print("Tempo gasto no LDA tomotopy: {}".format(time.time() - tempo))
   tempo = time.time()
-  
-  #print(mdl.summary())
 
+  print("")
+  print("Criando 5 topicos utilizando o LSA/SVD do sklearn")
+  #lsa / svd
+  sk_data = [" ".join(line) for line in all_data]
+  sk_train_data =  [" ".join(line) for line in train_data]
+  sk_test_data =  [" ".join(line) for line in test_data]
+  # tfidf = tf * idf, onde tf é a contagem e idf é N/df. N é a quantidade de documentos, df é o número de documentos em que o termo ocorre
+  vectorizer = TfidfVectorizer(stop_words='english', max_df = 0.5, smooth_idf=True).fit(sk_data)
+  x_train_svd = vectorizer.fit_transform(sk_train_data)
+  x_test_svd = vectorizer.fit_transform(sk_test_data)
+  # SVD represent documents and terms in vectors 
+  svd_model = TruncatedSVD(n_components=5, algorithm='randomized', n_iter=100, random_state=122)
+  svd_model.fit(x_train_svd)
+  terms = vectorizer.get_feature_names()
+  print("Topicos:")
+  for i, comp in enumerate(svd_model.components_):
+    terms_comp = zip(terms, comp)
+    sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=True)[:10]
+    print("") 
+    print('Top 10 palavras do topico #{}'.format(i))
+    print(sorted_terms)
+  print("")
+  print("Encaixando nos topicos o teste: ")
+  teste = svd_model.fit_transform(x_test_svd)
+  predicted_topics = [np.argsort(each)[::-1][0] for each in teste]
+  print(test_data[3])
+  print("Topico previsto: {}".format(predicted_topics[3]))
+  print("Tempo gasto no LSA/SVD: {}".format(time.time() - tempo))
+  tempo = time.time()
+
+    
 main()  
