dcf3436c55c4ad68d80499e88677ee27240d2835
Raffael M. Paranhos <raffaelmp@id.uff.br>
2021-05-04 00:34:02 -0300

    delete

--- Helper/Model.py
+++
@@ -1,8 +0,0 @@
-class Model(object):
-    """Model Class for Persisting"""
-    def __init__(self, hmm=[], textHelper=[], probValues =[]):
-        self.HMM = hmm
-        self.TextHelper = textHelper
-        self.ProbValues = probValues
-
-
